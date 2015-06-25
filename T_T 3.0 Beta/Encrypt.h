8089662feeb6df2d390f85ba0f8e034399306b49
manoboy <manoboy@noemail.com>
2015-06-25 16:48:45 -0300

    Option to toggle string obfuscation true or false. Default: false

--- T_T 3.0 Beta/Encrypt.h
+++ T_T 3.0 Beta/Encrypt.h
@@ -20,11 +20,27 @@ protected:
 };
 extern CEncrypt *Encrypt;
 
+// STRING OBFUSCATION SUPPORT:
+// true:  use encrypted strings
+// false: use normal strings
+//
+// Change the value only if you know what you are doing!
+#define STRING_OBFUSCATION false
 
+#if STRING_OBFUSCATION
 #ifndef _TempXOR
 #define _TempXOR(A) Encrypt->TempXOR(A ## "...\n\.\*\.")
 #endif
 
 #ifndef _OnceXOR
 #define _OnceXOR(A) Encrypt->OnceXOR(A ## "...\n\.\*\.")
+#endif
+#else
+#ifndef _TempXOR
+#define _TempXOR(A) A
+#endif
+
+#ifndef _OnceXOR
+#define _OnceXOR(A) A
+#endif
 #endif
